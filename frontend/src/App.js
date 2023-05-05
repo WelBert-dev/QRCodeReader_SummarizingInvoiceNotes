1b5c675eb6a0d091f4d80f9ffacb7e8c188878dd
WelBert-dev <wellison.bertelli@hotmail.com>
2023-05-05 17:37:45 -0300

    Substitui t3 pelo react app normal sem typescript

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        ...
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
