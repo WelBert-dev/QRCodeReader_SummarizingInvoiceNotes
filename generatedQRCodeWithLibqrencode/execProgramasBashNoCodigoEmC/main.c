f1ae149ea2ef20f63412650c790f3c66dc18ce2b
WelBert-dev <wellison.bertelli@hotmail.com>
2023-05-08 14:25:46 -0300

    add apresentação base do trabalho

---
+++ generatedQRCodeWithLibqrencode/execProgramasBashNoCodigoEmC/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    system("qrencode 'https://welbert-dev.github.io/firstPortifolioGithubPages-2022/#/certificates' -t UTF8");
+    
+    return 0;
+}
