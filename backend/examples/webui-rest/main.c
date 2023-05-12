02f64a083343706642fd04e981153b6c51511dd8
WelBert-dev <wellison.bertelli@hotmail.com>
2023-05-12 20:38:13 -0300

    ajusta problemas, porém API não sera + utilizada

--- backend/examples/webui-rest/main.c
+++ backend/examples/webui-rest/main.c
@@ -13,14 +13,26 @@
 
 #include <time.h>
 
+#include <curl/curl.h>
+
+#define API_KEY "AIzaSyDJ2ysDUfcerIWw16djY_THaNGRnPS39ys"
+#define UPLOAD_URL "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart"
+
 static const char *s_http_addr = "http://localhost:8000";  // HTTP port
 static const char *s_root_dir = "web_root";
+
+// Definições de Paths:
+char imgsDirPath[] = "/home/welbert/Documentos/github/QRCodeReader_SummarizingInvoiceNotes/backend/examples/webui-rest/images/";
+
+char content_type[] = "Content-Type: application/json\r\n";
+char access_control[] = "Access-Control-Allow-Origin: http://localhost:3000\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\nAccess-Control-Allow-Headers: content-type, mode\r\n\r\n";
+char content_length[64];
 
 static void generate_qrcode(const char* json_str, const char* filename) {
     // Formatar o comando para gerar o QR code
     printf("Antes de executar o comando: %s\n", json_str);
     char command[4096];
-    snprintf(command, 4096, "qrencode -o %s.png \"%s\"", filename, json_str);
+    snprintf(command, 4096, "qrencode -o %s%s.png \"%s\"", imgsDirPath, filename, json_str);
     printf("DEPOIS de executar o comando: %s\n", json_str);
 
     // Executar o comando no terminal e capturar a saída
@@ -34,37 +46,58 @@ static void generate_qrcode(const char* json_str, const char* filename) {
     printf("DEPOIS DE DAR CLOSE: %s\n", json_str);
 }
 
-// Retorna o tipo MIME do arquivo de caminho 'path'
-// Retorna NULL se o tipo MIME não for conhecido
-static const char *get_mime_type(const char *path) {
-  const char *ext = strrchr(path, '.');
-  if (!ext) {
-    return NULL;
-  }
-  if (strcmp(ext, ".html") == 0 || strcmp(ext, ".htm") == 0) {
-    return "text/html";
-  }
-  if (strcmp(ext, ".css") == 0) {
-    return "text/css";
-  }
-  if (strcmp(ext, ".js") == 0) {
-    return "application/javascript";
-  }
-  if (strcmp(ext, ".png") == 0) {
-    return "image/png";
-  }
-  if (strcmp(ext, ".jpg") == 0 || strcmp(ext, ".jpeg") == 0) {
-    return "image/jpeg";
-  }
-  return NULL;
-}
+// // Retorna o tipo MIME do arquivo de caminho 'path'
+// // Retorna NULL se o tipo MIME não for conhecido
+// static const char *get_mime_type(const char *path) {
+//   const char *ext = strrchr(path, '.');
+//   if (!ext) {
+//     return NULL;
+//   }
+//   if (strcmp(ext, ".html") == 0 || strcmp(ext, ".htm") == 0) {
+//     return "text/html";
+//   }
+//   if (strcmp(ext, ".css") == 0) {
+//     return "text/css";
+//   }
+//   if (strcmp(ext, ".js") == 0) {
+//     return "application/javascript";
+//   }
+//   if (strcmp(ext, ".png") == 0) {
+//     return "image/png";
+//   }
+//   if (strcmp(ext, ".jpg") == 0 || strcmp(ext, ".jpeg") == 0) {
+//     return "image/jpeg";
+//   }
+//   return NULL;
+// }
+
+// Função de callback para ler o arquivo de imagem
+// static size_t read_callback(void *ptr, size_t size, size_t nmemb, void *stream) {
+//     size_t retcode = fread(ptr, size, nmemb, stream);
+//     return retcode;
+// }
 
 static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
-  // mg_http_add_header(c, "Access-Control-Allow-Origin", "*");
-
+  //mg_http_add_header(c, "Access-Control-Allow-Origin", "*");
+  // struct mg_http_message *hm = (struct mg_http_message *) ev_data;
+  // printf("\n\n\nREQUISIÇÂO METHOD: %s\n\n\n", hm->method.ptr);
   if (ev == MG_EV_HTTP_MSG) {
     struct mg_http_message *hm = (struct mg_http_message *) ev_data;
-    if (mg_http_match_uri(hm, "/api/post")) {
+    printf("\n\n\nREQUISIÇÂO METHOD: %s\n\n\n", hm->method.ptr);
+    if (mg_http_match_uri(hm, "/api/post") && mg_vcmp(&hm->method, "OPTIONS") == 0) {
+      printf("Recebida requisição OPTIONS pré-voo\n");
+
+      // Definições dos atributos comuns nas respostas HTTP:
+      // char content_type[] = "Content-Type: application/json\r\n";
+      // char access_control[] = "Access-Control-Allow-Origin: http://localhost:3000\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\nAccess-Control-Allow-Headers: Content-type, mode\r\n\r\n";
+      // // char content_length[64];
+      
+      mg_http_reply(c, 204, "Content-Type: application/json\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\nAccess-Control-Allow-Headers: content-type, mode\r\n\r\n", "");
+      // mg_http_reply(c, 204, content_type, access_control, "");
+      return;
+    } 
+    else if (mg_http_match_uri(hm, "/api/post") && mg_vcmp(&hm->method, "POST") == 0) {
+      printf("\n\n\nREQUISIÇÂO METHOD: %s\n\n\n", hm->method.ptr);
 
       // impirmindo o json recebido no body:
       printf("Received JSON: %.*s\n", (int) hm->body.len, hm->body.ptr);
@@ -82,8 +115,11 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
         if (error_ptr != NULL) {
             printf("Error before: %s\n", error_ptr);
         }
-        mg_http_reply(c, 400, "Content-Type: application/json\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\nAccess-Control-Allow-Headers: Content-Type\r\n\r\n{\"msg\": \"Hello World!\"}", "{%Q:%s}\n",
-                      "error", "ERROR ao tentar extrair o JSON! ;-;");
+
+        mg_http_reply(c, 400, content_type, access_control, "Error ao tentar ler Json do body");
+
+        // mg_http_reply(c, 400, "Content-Type: application/json\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\nAccess-Control-Allow-Headers: Content-Type\r\n\r\n",
+        //               "error", "ERROR ao tentar extrair o JSON! ;-;");
 
         free(body);
         return;
@@ -97,8 +133,10 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
       if (msg == NULL || !cJSON_IsString(msg)) {
         printf("Error: invalid JSON\n");
 
-        mg_http_reply(c, 400, "Content-Type: application/json\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\nAccess-Control-Allow-Headers: Content-Type\r\n\r\n{\"msg\": \"Hello World!\"}", "{%Q:%s}\n",
-                      "error", "JSON Inválido! ;-;");
+        mg_http_reply(c, 400, content_type, access_control, "Error json lido porém inválido");
+
+        // mg_http_reply(c, 400, "Content-Type: application/json\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\nAccess-Control-Allow-Headers: Content-Type\r\n\r\n{\"msg\": \"Hello World!\"}", "{%Q:%s}\n",
+        //               "error", "JSON Inválido! ;-;");
 
         cJSON_Delete(root);
         free(body);
@@ -120,9 +158,9 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
       regex_t regex;
       regmatch_t pmatch[1];
       
-      if (regcomp(&regex, "/", REG_EXTENDED) != 0) {
+      if (regcomp(&regex, "[^[:alnum:]]", REG_EXTENDED) != 0) {
           printf("Erro na compilacao da expressao regular\n");
-          return 1;
+          return;
       }
       
       char *output = (char*) malloc(strlen(urlBefore));
@@ -143,28 +181,173 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
 
       generate_qrcode(msg_str, output);
 
+      // começa 
+
       int width, height;
       char pathTest[1024];
-      snprintf(pathTest, sizeof(pathTest), "%s%s.png", "/home/welbert/Documentos/github/QRCodeReader_SummarizingInvoiceNotes/backend/examples/webui-rest/", output);
+      snprintf(pathTest, sizeof(pathTest), "%s%s.png", imgsDirPath, output);
       printf("FILE PATH: %s\n", pathTest);
 
-      cJSON *json_response = cJSON_CreateObject();
-      cJSON_AddStringToObject(json_response, "path", pathTest);
-
-      // converter o objeto JSON em uma string
-      char *json_response_str = cJSON_Print(json_response);
-      cJSON_Delete(json_response);
-
-      mg_http_reply(c, 
-                    200, 
-                    "Content-Type: application/json\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\n",
-                    json_response_str);
-
-      free(json_response_str);
-
       free(output);
-
       regfree(&regex);
+
+      FILE *fp = fopen(pathTest, "rb");
+      if (fp != NULL) {
+        printf("\n\n\n%s\n\n\n", "FP NÂO È NULL\n");
+        // Obter o tamanho do arquivo
+        fseek(fp, 0L, SEEK_END);
+        size_t file_size = ftell(fp);
+        fseek(fp, 0L, SEEK_SET);
+
+        printf("\n\n\n%s\n\n\n","Alocando o buffer em memoria com o tamanho do file_size");
+        // Alocar um buffer para a imagem
+        char *buf = (char *)malloc(file_size);
+        if (buf == NULL) {
+          // mg_http_send_error(c, 500, "Internal Server Error", "");
+          mg_http_reply(c, 500, "Internal Server Error", "");
+          fclose(fp);
+          return;
+        }
+
+        // Ler a imagem no buffer
+        size_t bytes_read = fread(buf, 1, file_size, fp);
+        if (bytes_read != file_size) {
+          // mg_http_send_error(c, 500, "Internal Server Error", "");
+
+          mg_http_reply(c, 500, "Internal Server Error", "");
+
+          free(buf);
+          fclose(fp);
+          return;
+        }
+
+        // Printa buffer para ver se esta correto (em hexadecimal) e depois verificamos o hexa da imagem
+        for (int i = 0; i < bytes_read; i++) {
+          printf("%02x", (unsigned char) buf[i]);
+        }
+
+        // printf("\n\n\n%s\n\n\n", "CHEGOU NA TAREFA DO CURL!!!!!");
+        // Faz upload da imagem no google drive, pois esta dando problema enviando na resp da req a imagem em buffer:
+        // CURL *curl;
+        // CURLcode res;
+
+        // curl_global_init(CURL_GLOBAL_ALL);
+
+        // curl = curl_easy_init();
+        // if (curl) {
+        //     printf("\n\n\n%s\n\n\n", "CHEGOU NO IF QUE VERIFICA SE CURL CARREGOU CORRETAMENTE!!!!!");
+        //     struct curl_slist *headers = NULL;
+
+        //     // Definir os cabeçalhos de autenticação e tipo de conteúdo
+        //     headers = curl_slist_append(headers, "Authorization: Bearer AIzaSyDJ2ysDUfcerIWw16djY_THaNGRnPS39ys");
+        //     headers = curl_slist_append(headers, "Content-Type: image/png");
+
+        //     printf("\n\n\n%s\n\n\n", "DEFININDO OS DADOS DA REQUISIÇÂO POST!!!!!");
+        //     // Definir os dados da solicitação HTTP POST
+        //     curl_easy_setopt(curl, CURLOPT_POST, 1L);
+        //     curl_easy_setopt(curl, CURLOPT_URL, UPLOAD_URL);
+        //     printf("\n\n\n%s\n\n\n", "CARREGANDO IMAGEM EM BUFFER E SETANDO NO CURL!!!!!");
+        //     curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_callback);
+        //     printf("\n\n\n%s\n\n\n", "CARREGOU POIS PASSOU DA LINHA!!!!!");
+        //     curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+        //     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        //     curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE_LARGE, (curl_off_t)-1);
+        //     curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS);
+        //     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+        //     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+
+        //     printf("\n\n\n%s\n\n\n", "SETADO TUDO FINALMENTE EXECUTA A REQUISIÇÂO CURL!!!!!");
+        //     // Executar a solicitação HTTP POST
+        //     res = curl_easy_perform(curl);
+        //     printf("\n\n\n%s\n\n\n", "PASSOU DA LINHA QUE EXECUTA !!!!!");
+        //     if (res != CURLE_OK) {
+        //       printf("\n\n\n%s\n\n\n", "RESPOSTA DA REQUISIÇÂO FALHOU !!!!!");
+        //       fprintf(stderr, "curl_easy_perform() falhou: %s\n",
+        //               curl_easy_strerror(res));
+        //     }
+        //     printf("\n\n\n%s\n\n\n", "RESPOSTA DA REQUISIÇÂO OK !!!!!");
+
+        //     // Analisar a resposta da API do Google Drive
+        //     long response_code;
+        //     curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+        //     if (response_code == 200) {
+        //         printf("Upload de imagem PNG concluído com sucesso.\n");
+        //     } else {
+        //         printf("Erro ao fazer upload de imagem PNG. Código de resposta: %ld\n", response_code);
+        //     }
+
+        //     // Liberar memória
+        //     curl_slist_free_all(headers);
+        //     curl_easy_cleanup(curl);
+        // }
+
+        // curl_global_cleanup();
+
+        // Enviar a imagem como corpo da resposta HTTP
+        printf("%s", "\nANTES DO RESPONSEEEEE\n\n");
+
+        // mg_http_reply(c, 200, content_type, access_control, content_length, buf, bytes_read);
+
+        // size_t content_length_size = sizeof("\r\nContent-Length: \r\n") + 10; // 10 é o tamanho máximo para o valor
+        // char content_length[content_length_size];
+        // snprintf(content_length, content_length_size, "Content-Length: %zu\r\n", bytes_read);
+
+
+        printf("\n\n%s\n\n", content_length);
+
+        // int bytes_read_int = (int) bytes_read;
+        // snprintf(content_length, sizeof(content_length), "Content-Length: %d\r\n\0", bytes_read);
+        // printf("\n\n\n\n\nINTEIRO: %d\n\n\n\n\n", bytes_read_int);
+
+        // snprintf(content_length, sizeof(content_length), "%zu", file_size);
+
+        // mg_http_se(c, 200, file_size, "Content-Type: image/png\r\nContent-Length: %s\r\n", content_length);
+        // mg_http_send(c, buf, file_size);
+
+        // mg_http_reply(c, 
+        //               200, 
+        //               "Content-Type: image/png\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\nAccess-Control-Allow-Headers: Content-Type\r\n", content_length, buf);
+
+
+        // mg_http_reply(c, 
+        //               200, 
+        //               "Content-Type: image/png\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\nAccess-Control-Allow-Headers: Content-Type\r\n", 
+        //               buf, 
+        //               350);
+
+        free(buf);
+        fclose(fp);
+        
+        // mg_http_reply(c, 
+        //       200, 
+        //       "Content-Type: image/png\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\nAccess-Control-Allow-Headers: Content-Type\r\n\r\n",
+        //       content_length, buf, bytes_read);
+
+        return;
+      } else {
+        // Tratar erro de abertura de arquivo
+      }
+
+      // finaliza
+
+      // int width, height;
+      // char pathTest[1024];
+      // snprintf(pathTest, sizeof(pathTest), "%s%s", "/home/welbert/Documentos/github/QRCodeReader_SummarizingInvoiceNotes/backend/examples/webui-rest/images/", output);
+      // printf("FILE PATH: %s\n", pathTest);
+
+      // cJSON *json_response = cJSON_CreateObject();
+      // cJSON_AddStringToObject(json_response, "path", pathTest);
+
+      // // converter o objeto JSON em uma string
+      // char *json_response_str = cJSON_Print(json_response);
+      // cJSON_Delete(json_response);
+      
+      // mg_http_reply(c, 
+      //               200, 
+      //               "Content-Type: application/json\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\nAccess-Control-Allow-Headers: Content-Type\r\n\r\n",
+      //               json_response_str);
+
+      // free(json_response_str);
 
     } else {
       struct mg_http_serve_opts opts = {.root_dir = s_root_dir};
--- frontend/src/screens/WriterScreen.js
+++ frontend/src/screens/WriterScreen.js
@@ -1,37 +1,182 @@
 import React, { useState } from 'react';
 
+import imgTest from './httpsgithubcomWelBertdev.png';
+
 function WriterScreen() {
   
-  const [msg, setMsg] = useState('');
+  const [msg, setMsg] = useState(null);
+  const [imageSrc, setImageSrc] = useState('');
+  const [isValidImage, setIsValidImage] = useState(false);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     console.log("ENTROU NO HANDLE SUBMIT");
     console.log("MSG: ");
     console.log(msg);
     const url = `http://localhost:8000/api/post`;
-    fetch(url, {
-      method: 'POST',
-      mode: 'no-cors',
-      headers: {
-        "Content-type": "application/json; charset=UTF-8"
-      },
-      body: JSON.stringify({ msg: msg })
-    })
-    .then(response => response.arrayBuffer())
-    .then((buffer) => {
-      console.log("RESPONSE: ");
-      console.log(buffer);
-      const blob = new Blob([buffer], {type: 'image/png'});
-      const imgUrl = URL.createObjectURL(blob);
-      const img = new Image();
-      img.onload = function() {
-      // exibir a imagem em uma tag img no DOM
-      document.body.appendChild(img);
-      };
-      img.src = imgUrl;
-    })
-    .catch(error => console.error(error));
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+      "mode": "cors",
+      "Origin": "http://localhost:3000",
+      "Referer": "http://localhost:3000/",
+      "Access-Control-Request-Headers": "Content-type",
+      "Access-Control-Request-Method": "*",
+      // "Access-Control-Allow-Origin": "*",
+
+    },
+    body: JSON.stringify({ msg: msg })
+  })
+  .then(response => {
+    if (response.ok) {
+      const contentType = response.headers.get('Content-Type');
+
+      // Verificar o tipo de conteúdo e converter conforme necessário
+      if (contentType.includes('application/json')) {
+        return response.json();
+      } else if (contentType.includes('image/png') || contentType.includes('image/jpeg')) {
+        return response.blob();
+      } else {
+        return response.text();
+      }
+    } else {
+      throw new Error('Erro na solicitação: ' + response.status);
+    }
+  })
+  .then(data => {
+    // Manipular os dados conforme o tipo de conteúdo
+if (data instanceof Blob) {
+      // Dados da imagem
+      if (URL && typeof URL.createObjectURL === 'function') {
+        console.log("IMAGEM BLOB VÀLIDA ENTROU NO IF");
+        const imgURL = URL.createObjectURL(data);
+        // Realizar as ações necessárias com a imagem
+        console.log(imgURL);
+        // const imgElement = document.createElement('img');
+        // imgElement.setAttribute("src", imgURL);
+        setImageSrc(imgURL);
+
+        // Verificar se o blob é uma imagem válida
+        const isImage = data.type.startsWith('image/');
+        setIsValidImage(isImage);
+  
+        // const conatinerElement = document.getElementById("main-writer--container");
+        // conatinerElement.appendChild(imgElement);
+
+      } else {
+        throw new Error('O navegador não suporta a função createObjectURL.');
+      }
+    } else {
+      // Outro tipo de dado
+      console.log("OUTRO TIPO DE DADO: ");
+      console.log(data);
+    }
+  })
+  .catch(error => {
+    console.error(error);
+  });
+    
+    // await fetch(url, {
+      // method: 'POST',
+      //   headers: {
+      //     "Content-type": "application/json; charset=UTF-8",
+      //     "mode": "cors",
+      //     "Origin": "http://localhost:3000",
+      //     "Referer": "http://localhost:3000/",
+      //     "Access-Control-Request-Headers": "content-type",
+      //     "Access-Control-Request-Method": "*",
+
+      //   },
+      //   body: JSON.stringify({ msg: msg })
+    // })
+    // .then(response => {
+    //   if (response.status == 200 && response.headers.get('Content-Type').includes("image/png")){
+    //     console.log("DEU BÃO, segue RESPONSE:");
+    //     console.log(response);
+
+    //     const responseBlob = response.blob();
+
+    //     const imgUrl = URL.createObjectURL(responseBlob);
+  
+        // const imgElement = document.createElement('img');
+        // imgElement.src = imgUrl;
+  
+        // const conatinerElement = document.getElementById("main-writer--container");
+        // conatinerElement.appendChild(imgElement);
+
+    //   } else {
+    //     console.log("Deu bão mai ou menu, entrou no ELSE do if response.ok, segue RESPONSE: ");
+    //     console.log(response.headers.get('Content-Type'));
+
+    //   }
+    // })
+    // .catch(error => {
+    //   console.log("DEU RUIMZAO, caiu no catch... objeto RESPONSE Inexistente, segue error: ");
+    //   console.error(error)
+    // });
+
+    // try {
+    //   const response = await fetch(url, {
+    //     method: 'POST',
+    //     headers: {
+    //       "Content-type": "application/json; charset=UTF-8",
+    //       "mode": "cors",
+    //       "Origin": "http://localhost:3000",
+    //       "Referer": "http://localhost:3000/",
+    //       "Access-Control-Request-Headers": "content-type",
+    //       "Access-Control-Request-Method": "*",
+
+    //     },
+    //     body: JSON.stringify({ msg: msg })
+    //   });
+    //   console.log(response);
+    //   const responseBlob = await response.blob();
+
+    //   const imgUrl = URL.createObjectURL(responseBlob);
+
+    //   const imgElement = document.createElement('img');
+    //   imgElement.src = imgUrl;
+
+    //   const conatinerElement = document.getElementById("main-writer--container");
+    //   conatinerElement.appendChild(imgElement);
+
+    //   // Adicione a imagem ao elemento desejado na página
+    //   // document.body.appendChild(imgElement); 
+    //   // const img = document.getElementById('my-image');
+    //   // const rootProject = "./public/";
+
+
+    //   // const cleanedStr = msg.replace(/[^\w\s]/gi, '');
+    //   // console.log(cleanedStr);
+
+    //   // // path da img QR gerada:
+    //   // const pathOfImage = rootProject + cleanedStr + ".png";
+    //   // console.log(pathOfImage);
+
+    //   // fetch(pathOfImage, { method: 'HEAD' })
+    //   //   .then(response => {
+    //   //     if (response.ok) {
+    //   //       // console.log("DEU BAO");
+
+    //   //       // const objImgElement = document.createElement("img");
+    //   //       // objImgElement.src = pathOfImage;
+
+    //         // const objConatinerElement = document.getElementById("main-writer--container");
+    //         // objConatinerElement.appendChild(objImgElement);
+    //   //       setImageSrc(pathOfImage);
+    //   //     } else {
+    //   //       console.error(`Erro: o arquivo ${pathOfImage} não foi encontrado.`);
+    //   //     }
+    //   //   })
+    //   //   .catch(error => console.error(error));
+      
+
+    // } catch (error) {
+    //   console.log("ERRROOOOOOO");
+    //   console.error(error);
+    // }
 
     setMsg('');
   };
@@ -41,9 +186,9 @@ function WriterScreen() {
   };
 
   return (
-    <div className="main-writer--container">
+    <div id="main-writer--container" className="main-writer--container">
 
-      <form action="127.0.0.1:8000/api/post" method="POST"
+      <form action="http://localhost:8000/api/post" method="POST"
        onSubmit={handleSubmit}>
         <label>Mensagem: </label>
         <input type="text" name="msg" value={msg} onChange={handleChange}></input><br/>
@@ -52,6 +197,15 @@ function WriterScreen() {
         <input type="reset" value="Limpar"></input>
 
       </form>
+
+      {/* {imageSrc && <img src={imageSrc} alt="Imagem" />} */}
+
+      {isValidImage ? (
+        <img src={imageSrc} alt="Imagem" />
+      ) : (
+        <p>Imagem inválida</p>
+      )}
+      {/* <img src={imgTest} alt="pamonha"/> */}
     </div>
   );
 }
